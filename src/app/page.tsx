c283f6606ce625d65405ba2862a5c16377df36c0
Ema Pms <emapms@gmail.com>
2025-07-04 19:44:42 +0000

    Style.css body {   margin: 0;   background: radial-gradient(#0f0f0f, #00

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,11 @@
 "use client";
 
-import { useEffect, useRef } from "react";
 import ThreeDCard from "@/components/three-d-card";
 
 export default function Home() {
-  const mainRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const handleMouseMove = (event: MouseEvent) => {
-      if (mainRef.current) {
-        const { clientX, clientY } = event;
-        const rect = mainRef.current.getBoundingClientRect();
-        const x = clientX - rect.left;
-        const y = clientY - rect.top;
-        mainRef.current.style.setProperty("--mouse-x", `${x}px`);
-        mainRef.current.style.setProperty("--mouse-y", `${y}px`);
-      }
-    };
-
-    window.addEventListener("mousemove", handleMouseMove);
-
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
-
   return (
     <main
-      ref={mainRef}
-      className="dynamic-bg relative flex min-h-screen flex-col items-center justify-center overflow-hidden"
+      className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden"
     >
       <div className="relative h-80 w-full max-w-xl md:h-96">
         <ThreeDCard />
