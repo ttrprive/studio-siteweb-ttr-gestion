09327a628e120eaa6486dd32aa8c7d08c8ce173a
Ema Pms <emapms@gmail.com>
2025-07-04 22:04:28 +0000

    Quand toi même tu vois le résultat, c'est correct?

--- src/components/three-d-card.tsx
+++ src/components/three-d-card.tsx
@@ -41,7 +41,7 @@ const ThreeDCard: React.FC = () => {
         const logoTexture = textureLoader.load('/logo.svg', (texture) => {
             texture.anisotropy = renderer.capabilities.getMaxAnisotropy();
             texture.repeat.set(0.35, 0.35); 
-            texture.offset.set(0.325, -0.08);
+            texture.offset.set(0.325, -0.2);
             texture.needsUpdate = true;
         });
         logoTexture.colorSpace = THREE.SRGBColorSpace;
