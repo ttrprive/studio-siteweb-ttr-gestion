e68335801bb57526c3398fdae1ae57ee31718ad1
Ema Pms <emapms@gmail.com>
2025-07-04 21:26:22 +0000

    Le logo n'est pas au milieu, il doit descendre encore un peu

--- src/components/three-d-card.tsx
+++ src/components/three-d-card.tsx
@@ -41,7 +41,7 @@ const ThreeDCard: React.FC = () => {
         const logoTexture = textureLoader.load('/logo.svg', (texture) => {
             texture.anisotropy = renderer.capabilities.getMaxAnisotropy();
             texture.repeat.set(0.35, 0.35); 
-            texture.offset.set(0.325, 0.325);
+            texture.offset.set(0.325, 0.30);
             texture.needsUpdate = true;
         });
         logoTexture.colorSpace = THREE.SRGBColorSpace;
