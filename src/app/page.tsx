e9a18552248f17fd3a4b8215402e34f647ac3c15
Ema Pms <emapms@gmail.com>
2025-07-05 14:41:46 +0000

    Centre la carte

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import ThreeDCard from "@/components/three-d-card";
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden">
-      <div className="absolute inset-0 z-0">
+      <div className="absolute inset-0 z-0 flex items-center justify-center">
         <ThreeDCard />
       </div>
     </main>
