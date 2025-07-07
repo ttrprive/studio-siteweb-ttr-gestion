6d152a9e565ff00ac10e3cdd88bd7a714f30bcc7
Ema Pms <emapms@gmail.com>
2025-07-07 13:59:32 +0000

    Le titre doit simplement être '' TTR GESTION ''

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: 'ttrgestion.app',
+  title: 'TTR GESTION',
   description: 'La nouvelle ère de la gestion.',
 };
 
