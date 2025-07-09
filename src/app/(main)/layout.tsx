734cee51f0ac224c29ccb58684047d3f5a504264
Ema Pms <emapms@gmail.com>
2025-07-09 11:01:38 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <MainSidebar>
    
    wit

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,22 +1,7 @@
-import { SidebarProvider, Sidebar, SidebarInset } from "@/components/ui/sidebar";
-import { MainSidebar } from "@/components/main-sidebar";
-
 export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <SidebarProvider>
-      <Sidebar
-        side="right"
-        collapsible="icon"
-        variant="floating"
-        className="backdrop-blur-sm"
-      >
-        <MainSidebar />
-      </Sidebar>
-      <SidebarInset>{children}</SidebarInset>
-    </SidebarProvider>
-  );
+  return <>{children}</>;
 }
