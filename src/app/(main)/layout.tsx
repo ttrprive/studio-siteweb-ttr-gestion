312f04d084eec7728cad8d03ebc8dcd2a485749f
Ema Pms <emapms@gmail.com>
2025-07-09 13:44:58 +0000

    la page est toujours blanche

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,27 +1,33 @@
-import {MainSidebar} from '@/components/main-sidebar';
-import {
-  Sidebar,
-  SidebarInset,
-  SidebarProvider,
-  SidebarRail,
-} from '@/components/ui/sidebar';
+"use client";
+
+import { MainSidebar } from '@/components/main-sidebar';
+import { Sidebar, SidebarInset, SidebarProvider, SidebarRail } from '@/components/ui/sidebar';
+import React, { useState, useEffect } from 'react';
 
 export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <SidebarProvider>
-      <Sidebar
-        collapsible="icon"
-        variant="floating"
-        side="right"
-        className="hidden h-[320px] md:flex"
-      >
-        <MainSidebar />
-        <SidebarRail />
-      </Sidebar>
+      {isClient && (
+        <Sidebar
+          collapsible="icon"
+          variant="floating"
+          side="right"
+          className="hidden h-[320px] md:flex"
+        >
+          <MainSidebar />
+          <SidebarRail />
+        </Sidebar>
+      )}
       <SidebarInset>{children}</SidebarInset>
     </SidebarProvider>
   );
