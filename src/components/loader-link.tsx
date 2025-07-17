7dd4e01cf4e4833feea946c5736c7274dda1f300
Ema Pms <emapms@gmail.com>
2025-07-17 20:43:35 +0000

    encor un éhec, fait vite stp

--- src/components/loader-link.tsx
+++ src/components/loader-link.tsx
@@ -3,7 +3,7 @@
 import { useLoader } from "@/context/loader-context";
 import Link, { LinkProps } from "next/link";
 import { usePathname } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 
 type LoaderLinkProps = LinkProps & {
     children: React.ReactNode;
@@ -14,19 +14,16 @@ const LoaderLink: React.FC<LoaderLinkProps> = ({ children, href, className, ...p
     const { startLoading, stopLoading } = useLoader();
     const pathname = usePathname();
 
+    const handleClick = useCallback((e: React.MouseEvent<HTMLAnchorElement>) => {
+        if (pathname !== href.toString()) {
+            startLoading();
+        }
+    }, [pathname, href, startLoading]);
+
     useEffect(() => {
-        // This will stop the loader when the page navigation completes
         stopLoading();
     }, [pathname, stopLoading]);
-
-    const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-        // Don't start loader if it's the same page
-        if (pathname === href.toString()) {
-            return;
-        }
-        startLoading();
-    };
-
+    
     return (
         <Link href={href} onClick={handleClick} className={className} {...props}>
             {children}
