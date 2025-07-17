a6bff3dcd83bd7124431bf642da226f551cb4ff9
Ema Pms <emapms@gmail.com>
2025-07-17 19:50:19 +0000

    l'icone de compte ne s'est pas afficher alors que j'utilise le email adm

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,32 +3,24 @@
 import { AppFooter } from '@/components/app-footer';
 import { MainSidebar } from '@/components/main-sidebar';
 import { Sidebar, SidebarInset, SidebarProvider, SidebarRail } from '@/components/ui/sidebar';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
   return (
     <SidebarProvider>
-      {isClient && (
-        <Sidebar
-          collapsible="icon"
-          variant="floating"
-          side="right"
-          className="hidden h-[320px] md:flex z-50"
-        >
-          <MainSidebar />
-          <SidebarRail />
-        </Sidebar>
-      )}
+      <Sidebar
+        collapsible="icon"
+        variant="floating"
+        side="right"
+        className="hidden h-[320px] md:flex z-50"
+      >
+        <MainSidebar />
+        <SidebarRail />
+      </Sidebar>
       <SidebarInset>
         <div className="flex flex-col min-h-screen">
           <main className="flex-grow">{children}</main>
