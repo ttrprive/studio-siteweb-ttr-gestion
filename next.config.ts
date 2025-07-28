c269218a8b04fd7bbe333abeb2d2aca72ceca2c5
Ema Pms <emapms@gmail.com>
2025-07-28 04:35:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,17 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: "",
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "",
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: "",
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "",
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "",
+    NEXT_PUBLIC_FIREBASE_APP_ID: "",
+    CLOUDINARY_CLOUD_NAME: "",
+    CLOUDINARY_API_KEY: "",
+    CLOUDINARY_API_SECRET: "",
+  },
 };
 
 export default nextConfig;
