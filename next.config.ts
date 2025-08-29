a96a6bbf5aeb8cf13c440d80747101738de5687c
Ema Pms <emapms@gmail.com>
2025-08-29 23:49:51 +0000

    le site ne se met toujours pas ajour malgré tes efforts;  regarde ds le

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,6 @@ require('dotenv').config({ path: './.env.local' });
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
