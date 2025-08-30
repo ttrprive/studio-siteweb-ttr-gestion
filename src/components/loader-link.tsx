d7af651a5ffca2cd23aa93070d9c962abfb41efb
Ema Pms <emapms@gmail.com>
2025-08-30 11:03:41 +0000

    Warning: Function components cannot be given refs. Attempts to access th

--- src/components/loader-link.tsx
+++ src/components/loader-link.tsx
@@ -1,39 +1,45 @@
+
 "use client";
 
 import { useLoader } from "@/context/loader-context";
 import Link, { LinkProps } from "next/link";
 import { usePathname } from "next/navigation";
-import React, { useEffect, useCallback, MouseEvent } from "react";
+import React, { useEffect, useCallback, MouseEvent, forwardRef, Ref } from "react";
 
-type LoaderLinkProps = LinkProps & {
-    children: React.ReactNode;
+type LoaderLinkProps = Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps> & LinkProps & {
+    children?: React.ReactNode;
     className?: string;
     onClick?: (e: MouseEvent<HTMLAnchorElement>) => void;
 };
 
-const LoaderLink: React.FC<LoaderLinkProps> = ({ children, href, className, onClick, ...props }) => {
-    const { startLoading, stopLoading } = useLoader();
-    const pathname = usePathname();
+const LoaderLink = forwardRef<HTMLAnchorElement, LoaderLinkProps>(
+    ({ children, href, className, onClick, ...props }, ref) => {
+        const { startLoading, stopLoading } = useLoader();
+        const pathname = usePathname();
 
-    const handleClick = useCallback((e: MouseEvent<HTMLAnchorElement>) => {
-        if (onClick) {
-            onClick(e);
-        }
-        if (href.toString() !== pathname) {
-            startLoading();
-        }
-    }, [href, pathname, startLoading, onClick]);
+        const handleClick = useCallback((e: MouseEvent<HTMLAnchorElement>) => {
+            if (onClick) {
+                onClick(e);
+            }
+            // Only show loader if navigating to a different page
+            if (href.toString() !== pathname) {
+                startLoading();
+            }
+        }, [href, pathname, startLoading, onClick]);
 
-    useEffect(() => {
-        // Stop loading whenever the pathname changes
-        stopLoading();
-    }, [pathname, stopLoading]);
-    
-    return (
-        <Link href={href} onClick={handleClick} className={className} {...props}>
-            {children}
-        </Link>
-    );
-};
+        useEffect(() => {
+            // Stop loading whenever the pathname changes
+            stopLoading();
+        }, [pathname, stopLoading]);
+        
+        return (
+            <Link href={href} onClick={handleClick} className={className} {...props} ref={ref}>
+                {children}
+            </Link>
+        );
+    }
+);
+
+LoaderLink.displayName = "LoaderLink";
 
 export default LoaderLink;
