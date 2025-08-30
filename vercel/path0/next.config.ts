3632cedc5cf4ae8ff39c6b7eb67b2964a3866457
Ema Pms <emapms@gmail.com>
2025-08-30 10:25:12 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ vercel/path0/next.config.ts
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+
+require('dotenv').config({ path: './.env.local' });
+
+const nextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+module.exports = nextConfig;
