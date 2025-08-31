a3698d0325c05ddb5bdc04c24dd329b6a4a296fc
Ema Pms <emapms@gmail.com>
2025-08-31 17:04:19 +0000

    maitenant tu est sur que ça marchera?

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -447,6 +447,9 @@ const SidebarGroupAction = ({
       className={cn(
         "absolute right-3 top-3.5 flex aspect-square w-5 items-center justify-center rounded-md p-0 text-sidebar-foreground outline-none ring-sidebar-ring transition-transform hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0",
         "after:absolute after:-inset-2 after:md:hidden",
+        "peer-data-[size=sm]/menu-button:top-1",
+        "peer-data-[size=default]/menu-button:top-1.5",
+        "peer-data-[size=lg]/menu-button:top-2.5",
         "group-data-[collapsible=icon]:hidden",
         className
       )}
