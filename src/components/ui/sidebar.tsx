6b1e4e63020391bddd38e9bd7e45da4f4220f9f3
Ema Pms <emapms@gmail.com>
2025-08-31 14:47:07 +0000

    sincèrement je commence par etre déçu, j'ai d'autre chose afaire
    [14:41

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -420,14 +420,14 @@ const SidebarGroup = React.forwardRef<
 SidebarGroup.displayName = "SidebarGroup"
 
 const SidebarGroupLabel = React.forwardRef<
-  HTMLDivElement,
+  HTMLElement,
   React.ComponentProps<"div"> & { asChild?: boolean }
 >(({ className, asChild = false, ...props }, ref) => {
   const Comp = asChild ? Slot : "div"
 
   return (
     <Comp
-      ref={ref}
+      ref={ref as React.Ref<HTMLDivElement>}
       data-sidebar="group-label"
       className={cn(
         "duration-200 flex h-8 shrink-0 items-center rounded-md px-2 text-xs font-medium text-sidebar-foreground/70 outline-none ring-sidebar-ring transition-[margin,opa] ease-linear focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0",
